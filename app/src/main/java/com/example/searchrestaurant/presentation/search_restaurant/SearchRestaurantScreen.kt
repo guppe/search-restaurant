c3f0cbf0d8d57ce42c11f65bf453bd92cfd8045a
guppe <mst39639toi8@gmail.com>
2023-02-17 16:43:00 +0900

    presentationの設定変更

--- app/src/main/java/com/example/searchrestaurant/presentation/search_restaurant/SearchRestaurantScreen.kt
+++ app/src/main/java/com/example/searchrestaurant/presentation/search_restaurant/SearchRestaurantScreen.kt
@@ -1,11 +1,33 @@
 package com.example.searchrestaurant.presentation.search_restaurant
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.*
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun SearchRestaurantScreen(
-    viewModel: SearchRestaurantViewModel = hiltViewModel()
+    navController: NavController,
+    searchRestaurantViewModel: SearchRestaurantViewModel = viewModel()
 ) {
+    LaunchedEffect(Unit) {
+        searchRestaurantViewModel.searchRestaurant("1", "34.67", "135.52", "4", "json")
+    }
 
+    Scaffold {
+        val observeRestaurant = searchRestaurantViewModel.states.observeAsState()
+        observeRestaurant.value?.let { states ->
+            LazyColumn {
+
+                
+            }
+        }
+        
+    }
 }
--- app/src/main/java/com/example/searchrestaurant/presentation/search_restaurant/SearchRestaurantViewModel.kt
+++ app/src/main/java/com/example/searchrestaurant/presentation/search_restaurant/SearchRestaurantViewModel.kt
@@ -1,9 +1,33 @@
 package com.example.searchrestaurant.presentation.search_restaurant
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.searchrestaurant.model.Restaurant
+import com.example.searchrestaurant.repository.RestaurantRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class SearchRestaurantViewModel(): ViewModel() {
+
+    private val repository = RestaurantRepository()
+
+    private val _state = MutableLiveData<List<Restaurant>>()
+    val states: LiveData<List<Restaurant>> = _state
+
+    fun searchRestaurant(
+        range: String,
+        lat: String,
+        lng: String,
+        order: String,
+        format: String,
+    ){
+        viewModelScope.launch {
+            val result = repository.searchRestaurant(range, lat, lng, order, format)
+            _state.postValue(result ?: listOf())
+        }
+    }
 
 }
